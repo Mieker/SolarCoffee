7301da93bc433c0a2a5040e564ce741a3ab290ca
Marcin <mieker@wp.pl>
2020-11-01 14:24:06 +0100

    orderService under implementation

--- solarcoffee.Services/Inventory/InventoryService.cs
+++ solarcoffee.Services/Inventory/InventoryService.cs
@@ -1,33 +1,104 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SolarCoffee.Data;
 using SolarCoffee.Data.Models;
 
 namespace SolarCoffee.Services.Inventory
 {
     public class InventoryService : IInventoryService
     {
+        private readonly SolarDbContext _db;
+        private readonly ILogger<InventoryService> _logger;
+
+        public InventoryService(SolarDbContext dbContext, ILogger<InventoryService> logger)
+        {
+            _db = dbContext;
+            _logger = logger;
+        }
+
         public List<ProductInventory> GetCurrentInventory()
         {
-            throw new System.NotImplementedException();
+            return _db.ProductInventories
+                .Include(pi => pi.Product)
+                .Where(pi => !pi.Product.IsArchived)
+                .ToList();
         }
 
         public ServiceResponse<ProductInventory> UpdateUnitsAvailable(int id, int adjustment)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var inventory = _db.ProductInventories
+                    .Include(inv => inv.Product)
+                    .First(inv => inv.Product.Id == id);
+
+                inventory.QuantityOnHand += adjustment;
+
+                try
+                {
+                    CreateSnapshot(inventory);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError("Error creating inventory snapshot.");
+                    _logger.LogError(e.StackTrace);
+                }
+
+                _db.SaveChanges();
+                
+                return new ServiceResponse<ProductInventory>
+                {
+                    IsSuccess = true,
+                    Data = inventory,
+                    Message = $"Product {id} inventory adjusted",
+                    Time = DateTime.UtcNow
+                };
+            }
+            catch (Exception e)
+            {
+                return new ServiceResponse<ProductInventory>
+                {
+                    IsSuccess = false,
+                    Data = null,
+                    Message = e.StackTrace,
+                    Time = DateTime.UtcNow
+                };
+            }
         }
 
         public ProductInventory GetByProductId(int productId)
         {
-            throw new System.NotImplementedException();
-        }
+            var productInventory = _db.ProductInventories
+                .Include(inv => inv.Product)
+                .FirstOrDefault(inv => inv.Product.Id == productId);
 
-        public void CreateSnapshot()
-        {
-            throw new System.NotImplementedException();
+            return productInventory;
         }
-
+        
         public List<ProductInventorySnapshot> GetSnapshotHistory()
         {
-            throw new System.NotImplementedException();
+            var earliest = DateTime.UtcNow - TimeSpan.FromHours(6);
+            
+            return _db.ProductInventorySnapshots
+                .Include(snap => snap.Product)
+                .Where(
+                    snap => snap.SnapshotTime > earliest 
+                            && !snap.Product.IsArchived)
+                .ToList();
+        }
+
+        private void CreateSnapshot(ProductInventory inventory)
+        {
+            var snapshot = new ProductInventorySnapshot
+            {
+                SnapshotTime = DateTime.UtcNow,
+                QuantityOnHand = inventory.QuantityOnHand,
+                Product = inventory.Product
+            };
+            _db.Add(snapshot);
         }
     }
 }
--- solarcoffee.Services/Order/OrderService.cs
+++ solarcoffee.Services/Order/OrderService.cs
@@ -1,13 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SolarCoffee.Data;
 using SolarCoffee.Data.Models;
 
 namespace SolarCoffee.Services.Order
 {
     public class OrderService : IOrderService
     {
+        private readonly SolarDbContext _db;
+        private readonly ILogger<OrderService> _logger;
+
+        public OrderService(SolarDbContext db, ILogger<OrderService> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+        
         public List<SalesOrder> GetOrders()
         {
-            throw new System.NotImplementedException();
+            return _db.SalesOrders
+                .Include(order => order.Customer)
+                .ThenInclude(customer => customer.PrimeAddress)
+                .Include(order => order.SalesOrderItems)
+                .ToList();
         }
 
         public ServiceResponse<bool> GenerateInvoiceForOrder(SalesOrder order)
@@ -17,7 +35,31 @@ namespace SolarCoffee.Services.Order
 
         public ServiceResponse<bool> MarkFulfilled(int id)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var order = _db.SalesOrders.Find(id);
+                order.IsPaid = true;
+                _db.SalesOrders.Update(order);
+                _db.SaveChanges();
+                return new ServiceResponse<bool>
+                {
+                    Time = DateTime.UtcNow,
+                    IsSuccess = true,
+                    Message = "Orders fulfilled marked",
+                    Data = true
+                };
+            }
+            catch (Exception e)
+            {
+                return new ServiceResponse<bool>
+                {
+                    Time = DateTime.UtcNow,
+                    IsSuccess = false,
+                    Message = e.StackTrace,
+                    Data = false
+                };
+            }
+            
         }
     }
 }
